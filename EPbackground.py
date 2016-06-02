ce4d6ee7235f7a53643a1e4aa8200a41ade521a2
Zihao <zihaozhu@utexas.edu>
2016-06-01 23:56:44 -0500

    Finished prototype

--- EPbackground.py
+++ EPbackground.py
@@ -1,6 +1,9 @@
 import subprocess
 import os
 from socket import timeout
+import glob
+import time
+import schedule
 import re
 import urllib
 from urllib.request import urlopen
@@ -18,7 +21,7 @@ photos=[]
 #grabs the mac username for directory path
 username = os.getlogin()
 path = "/Users/%s/Desktop/EP/" % username
-
+picturesList = []
 def folder_exist():
 
     if(os.path.exists(path)):
@@ -28,50 +31,77 @@ def folder_exist():
 
 def downloadPhotos():
     counter=0
+    imgurUrlPattern = re.compile(r'(http://imgur.com/(.*))(\?.*)?')
     for i in photos:
 
         fileLocation = os.path.join(path,str(counter)+".jpg")
         if(not os.path.isfile(fileLocation)):
-            urllib.request.urlretrieve(i,fileLocation)
+            opener=urllib.request.URLopener()
+            opener.addheader('User-Agent','whatever')
+            if 'flickr' in i:
+                continue
+            if 'http' not in i:
+                continue
+            if 'http://imgur.com/' in i:
+                mo =imgurUrlPattern.search(i)
+                imgurFilename = mo.group(2)
+                i = "http://i.imgur.com/"+ imgurFilename+".jpg"
+#                if '?' in imgurFilename:
+#                    imgurFilename = imgurFilename[:imgurFilename.find('?')]
+
+            opener.retrieve(i,fileLocation)
             counter+=1
 
 def findPhotos():
     link="https://www.reddit.com/r/EarthPorn/"
-    try:
-        page = urlopen(link)
-    except urllib.error.URLError:
-        print("URL error. Exiting...")
-        return
-    except urllib.error.HTTPError:
-        print("HTTP error. Exiting...")
-        return
-    except timeout:
-        print("Timed out")
-        return
+    page = None
+    while page is None:
+        try:
+            page = urlopen(link)
+        except urllib.error.URLError:
+            print("URL error. Exiting...")
+
+        except urllib.error.HTTPError:
+            print("HTTP error. Exiting...")
+
+        except timeout:
+            print("Timed out")
+
     soup = bs(page.read(),"html.parser")
     pictures = soup.find_all('a', {'class':'thumbnail'}, href=True)
 
     for i in pictures:
         if(i['href'][0:4]=="http"):
             photos.append(i['href'])
-
-"""
-
-def create_photo_Folder():
-    if(not photo_exist()):
-        os.mkdir(path, 0755)
-
-
-def helpMessage():
-
-def set_desktop_background(filename):
-    subprocess.Popen(SCRIPT%filename, shell=True)
-
-"""
-
-def main():
-    folder_exist()
-    findPhotos()
     downloadPhotos()
-if __name__=='__main__':
-    main()
+
+def deletePhotos():
+    for photo in picturesList:
+        os.remove(photo)
+    photos[:]=[]
+    initiate()
+
+def findPicturesList():
+    picturesList = glob.glob(path+"*.jpg")
+    return picturesList
+def set_desktop_background():
+    picturesList=findPicturesList()
+    print(picturesList)
+    for filename in picturesList:
+        subprocess.Popen(SCRIPT%filename, shell=True)
+        print("set at "+filename)
+        time.sleep(3598)
+    deletePhotos()
+
+def initiate():
+    #check if folder exists, if it doesnt, it creates one
+    folder_exist()
+    #Finds the photos and download them. Try until success
+    findPhotos()
+    set_desktop_background()
+initiate()
+schedule.every().minutes.do(set_desktop_background)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
